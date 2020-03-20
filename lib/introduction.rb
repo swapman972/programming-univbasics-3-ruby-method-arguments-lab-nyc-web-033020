1e48a525d0c8563d37e9dfb1faab3e0ef3bf3965
Jordan Melidor-Fuxis <jordan.melidor@hotmail.fr>
2020-03-20 18:28:17 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -9,7 +9,10 @@ def introduction (name)
 end
 
 def introduction_with_language (name, language)
-  puts 
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+def introduction_with_language_optional (name, )
 
 
 
